cf955462c849d2116118550688a27f0c9518942a
achin1992 <achinsaxena92@gmail.com>
2023-06-03 16:03:05 +0530

    Delete app_idea_feed directory

--- app_idea_feed/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from app_idea_feed import models
-
-admin.site.register(models.IdeaFeedItem)
--- app_idea_feed/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppIdeaFeedConfig(AppConfig):
-    name = 'app_idea_feed'
