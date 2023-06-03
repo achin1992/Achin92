01e314a98509e926163199e61cb7ac2b7b3c2f54
achin1992 <achinsaxena92@gmail.com>
2023-06-03 16:02:48 +0530

    Delete app_user_actions directory

--- app_user_actions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from app_user_actions import models
-
-admin.site.register(models.UserLikedIdeas)
-admin.site.register(models.UserFavouriteIdeas)
-admin.site.register(models.UserCommentIdeas)
--- app_user_actions/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppUserActionsConfig(AppConfig):
-    name = 'app_user_actions'
