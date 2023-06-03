bd0e7f1d8d5f19975214580691493f15da5992a1
achin1992 <achinsaxena92@gmail.com>
2023-06-03 16:03:39 +0530

    Delete user_profiles_api directory

--- user_profiles_api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from user_profiles_api import models
-
-
-admin.site.register(models.UserProfile)
-admin.site.register(models.ProfileFeedItem)
--- user_profiles_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UserProfilesApiConfig(AppConfig):
-    name = 'user_profiles_api'
