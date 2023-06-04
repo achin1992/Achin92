6733801a10465959fa856950cee769ca208f6572
achin1992 <achinsaxena92@gmail.com>
2023-06-04 19:30:15 +0530

    Further improvement with exception handling

--- ChatBot/serviceNow_chatbot.py
+++ ChatBot/serviceNow_chatbot.py
@@ -9,11 +9,18 @@ os.environ['OPENAI_API_KEY'] = "sk-elDlL6fMUxO2kKpJ8cW2T3BlbkFJOoBz5o8HLcETFJZE1
 load_dotenv()
 
 problem_data_dir, index_name = "problemData", "problems_index"
+storage_context_directory = 'storage'
 
 
 def initialize_index(directory_path, index_id="problems_index"):
-    storage_context = StorageContext.from_defaults(persist_dir='storage')
-    if storage_context.index_store.get_index_struct(index_id) is None:
+    storage_context_exist = False
+    try:
+        storage_context = StorageContext.from_defaults(persist_dir=storage_context_directory)
+        storage_context_exist = True
+    except FileNotFoundError as e:
+        print("Storage directory doesn't seem to exist, need to construct the index")
+
+    if (not storage_context_exist) or (storage_context.index_store.get_index_struct(index_id) is None):
         print("Constructing index with name: " + index_id)
         max_input_size = 4096
         num_outputs = 100
@@ -28,13 +35,13 @@ def initialize_index(directory_path, index_id="problems_index"):
         index = GPTVectorStoreIndex.from_documents(documents=documents, service_context=service_context)
 
         index.set_index_id(index_id)
-        index.storage_context.persist('./storage')
+        index.storage_context.persist('./'+storage_context_directory)
     else:
         print("Index already present in storage index. No need to reconstruct it index: " + index_id)
 
 
 def problem_solution_bot(query, chatHistory=[]):
-    storage_context = StorageContext.from_defaults(persist_dir='storage')
+    storage_context = StorageContext.from_defaults(persist_dir=storage_context_directory)
     index = load_index_from_storage(storage_context, index_id=index_name)
     query_engine = index.as_query_engine()
     response = query_engine.query(query)
